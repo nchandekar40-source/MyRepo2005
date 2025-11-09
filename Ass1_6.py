6979dc726e8e51decd9b99ab0968241f789f0e09
namrata chandekar <namratachandekar9-netizen@gmail.com>
2025-11-09 06:17:52 -0800

    first commit

---
+++ Ass1_6.py
@@ -0,0 +1,8 @@
+#program for principal, rate, time, and simple intrest#
+p = float(input("Enter principal: "))
+r = float(input("Enter rate: "))
+t = float(input("Enter time: "))
+
+si = p * r * t / 100
+
+print("Simple Interest =", si)
